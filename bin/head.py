b4c9482a36e6c6c00e3ed59ad012579fc1b8cd6e
ceramisch <ceramisch@c68e8873-8163-41b3-892e-79309705a67f>
2010-04-13 15:38:03 +0000

    Generalized the wc, head and tail scripts. Now they can read/write all types of XML files, i.e. while before we had two separate scripts for counting the lines in a corpus or in a candidate list, now we have a single script that dinamically decides what type of file was input. Therefore, we introduced the notion of "entity" that might be a dictionary entry, a sentence or a candidate. An entity must be a subclass (direct or indirect) of Ngram.

--- bin/head.py
+++ bin/head.py
@@ -2,32 +2,33 @@
 # -*- coding:UTF-8 -*-
 
 ################################################################################
-#
+# 
 # Copyright 2010 Carlos Ramisch
-#
-# tail.py is part of mwetoolkit
-#
+# 
+# head.py is part of mwetoolkit
+# 
 # mwetoolkit is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
+# 
 # mwetoolkit is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
 # along with mwetoolkit.  If not, see <http://www.gnu.org/licenses/>.
-#
+# 
 ################################################################################
+#!/usr/bin/python
 """
-    Prints the last N entities of a list. Works like the "tail" command in
+    Prints the first N entities of a list. Works like the "head" command in
     the unix platform, only it takes a file in xml format as input.
-
-    This script is DTD independent, that is, it might be called on a corpus
+    
+    This script is DTD independent, that is, it might be called on a corpus 
     file, on a list of candidates or on a dictionary.
-
+    
     For more information, call the script with no parameter and read the
     usage instructions.
 """
@@ -37,17 +38,17 @@ import xml.sax
 import pdb
 
 from xmlhandler.genericXMLHandler import GenericXMLHandler
-from util import usage, read_options, treat_options_simplest, verbose
+from util import usage, read_options, treat_options_simplest
 
 ################################################################################
 # GLOBALS
 
-usage_string = """Usage: 
-    
+usage_string = """Usage:
+
 python %(program)s OPTIONS <file.xml>
 
-    OPTIONS may be:    
-    
+    OPTIONS may be:
+
 -n OR --number
     Number of entities that you want to print out. Default value is 10.
 
@@ -58,7 +59,6 @@ python %(program)s OPTIONS <file.xml>
 """
 limit = 10
 entity_counter = 0
-entity_buffer = [ None ] * limit
 
 ################################################################################
 
@@ -69,64 +69,39 @@ def treat_meta( meta ) :
         @param meta The `Meta` header that is being read from the XML file.
     """
 
-    print meta.to_xml().encode( 'utf-8' ) 
+    print meta.to_xml().encode( 'utf-8' )
 
 ################################################################################
-       
+
 def treat_entity( entity ) :
     """
-        For each entity in the corpus, puts it in a circular buffer. This is
-        necessary because we do not know the total number of lines, so we always
-        keep the last n lines in the global buffer.
-        
-        @param entity A subclass of `Ngram` that is being read from the XM.
-    """
-    global entity_counter, entity_buffer, limit
-    if limit > 0 :
-        entity_buffer[ entity_counter % limit ] = entity
-        entity_counter += 1
-    
-################################################################################    
+        For each entity in the file, prints it if the limit is still not
+        achieved. No buffering as in tail, this is not necessary here.
 
-def print_entities() :
+        @param entity A subclass of `Ngram` that is being read from the XML.
     """
-        After we read all the XML file, we can finally be sure about which lines
-        need to be printed. Those correspond exactly to the N last lines added
-        to the buffer.
-    """
-    global entity_buffer, entity_counter
-    for i in range( limit ) :
-        #pdb.set_trace()
-        # entity_buffer is a circular buffer. In order to print the entities in
-        # the correct order, we go from the cell imediately after the last one
-        # stored in the buffer (position entity_counter) until the until the
-        # last one stored in the buffer (position entity_counter-1). If there
-        # are less entities in the file than the limit, this padding is not
-        # needed and we simply go from 0 until entity_counter-1
-        index = ( entity_counter + i ) % min( limit, entity_counter )
-        if entity_buffer[ index ] != None :
-            print entity_buffer[ index ].to_xml().encode( 'utf-8' )
-        else :
-            break
-    
-################################################################################  
+    global entity_counter, limit
+    if entity_counter < limit :
+        print entity.to_xml()
+    entity_counter += 1
+
+################################################################################
 
 def treat_options( opts, arg, n_arg, usage_string ) :
     """
         Callback function that handles the command line options of this script.
-        
+
         @param opts The options parsed by getopts. Ignored.
-        
+
         @param arg The argument list parsed by getopts.
-        
-        @param n_arg The number of arguments expected for this script.    
+
+        @param n_arg The number of arguments expected for this script.
     """
-    global limit,  entity_buffer
+    global limit
     for ( o, a ) in opts:
         if o in ("-n", "--number") :
             try :
                 limit = int( a )
-                entity_buffer = [ None ] * limit
                 if limit < 0 :
                     raise ValueError
             except ValueError :
@@ -134,30 +109,29 @@ def treat_options( opts, arg, n_arg, usage_string ) :
                                      "integer value as argument of -n option."
                 usage( usage_string )
                 sys.exit( 2 )
-                             
+
     treat_options_simplest( opts, arg, n_arg, usage_string )
-    
-################################################################################    
+
+################################################################################
 # MAIN SCRIPT
 
 longopts = [ "verbose", "number=" ]
 arg = read_options( "vn:", longopts, treat_options, -1, usage_string )
-try :    
+try :
     print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
     print "<!DOCTYPE corpus SYSTEM \"dtd/mwttoolkit-corpus.dtd\">"
     print "<corpus>"
     parser = xml.sax.make_parser()
     parser.setContentHandler( GenericXMLHandler( treat_meta=treat_meta,
                                                  treat_entity=treat_entity ) )
-    if len( arg ) == 0 :        
+    if len( arg ) == 0 :
         parser.parse( sys.stdin )
     else :
-        for a in arg : 
-            input_file = open( a )            
+        for a in arg :
+            input_file = open( a )
             parser.parse( input_file )
-            input_file.close() 
-    print_entities() 
-    print "</corpus>"    
+            input_file.close()
+    print "</corpus>"
 except IOError, err :
     print >> sys.stderr, err
 except Exception, err :
--- bin/wc.py
+++ bin/wc.py
@@ -1,76 +1,102 @@
 #!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+################################################################################
+#
+# Copyright 2010 Carlos Ramisch
+#
+# wc.py is part of mwetoolkit
+#
+# mwetoolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# mwetoolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mwetoolkit.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
 """
-    This script simply gives some stats about a corpus file, such as number
+    This script simply gives some stats about a XML file, such as number
     of words, etc. Output is written on stderr.
+
+    This script is DTD independent, that is, it might be called on a corpus
+    file, on a list of candidates or on a dictionary.
     
     For more information, call the script with no parameter and read the
     usage instructions.
 """
 
 import sys
-import getopt
 import xml.sax
+import pdb
 
-from xmlhandler.dictXMLHandler import DictXMLHandler
-from xmlhandler.corpusXMLHandler import CorpusXMLHandler
-from xmlhandler.candidatesXMLHandler import CandidatesXMLHandler
-from util import read_options, treat_options_simplest, set_verbose, verbose
+from xmlhandler.genericXMLHandler import GenericXMLHandler
+from util import read_options, treat_options_simplest, verbose
      
 ################################################################################     
 # GLOBALS     
      
 usage_string = """Usage: 
     
-python %(program)s <candidates.xml>
+python %(program)s OPTIONS <file.xml>
 
-    The <corpus.xml> file must be valid XML (mwetoolkit-corpus.dtd). 
+    OPTIONS may be:
+
+
+-v OR --verbose
+    Print messages that explain what is happening.
+
+    The <file.xml> file must be valid XML (dtd/mwetoolkit-*.dtd).
 """    
 char_counter = 0
 word_counter = 0
-ngram_counter = 0
+entity_counter = 0
  
 ################################################################################     
        
-def treat_ngram( ngram ) :
+def treat_entity( entity ) :
     """
         For each candidate/sentence, counts the number of occurrences, the 
         number of words and the number of characters (except spaces and XML).
         
-        @param candidate The `Ngram` that is being read from the XML file.    
+        @param ngram A subclass of `Ngram` that is being read from the XML.
     """
-    global char_counter, word_counter, ngram_counter
+    global char_counter, word_counter, entity_counter
     
-    if ngram_counter % 100 == 0 :
-        verbose( "Processing ngram number %(n)d" % { "n":ngram_counter } )            
+    if entity_counter % 100 == 0 :
+        verbose( "Processing ngram number %(n)d" % { "n":entity_counter } )
 
-    for word in sentence.word_list :
+    for word in entity :
         word_counter += 1
         char_counter += len( word )
-    ngram_counter += 1
+    entity_counter += 1
 
 ################################################################################     
 # MAIN SCRIPT
 
 arg = read_options( "v", ["verbose"], treat_options_simplest, 1, usage_string )
-
 try :    
     input_file = open( arg[ 0 ] )        
     parser = xml.sax.make_parser()
     
-    corpusHandler = CorpusXMLHandler( treat_sentence=treat_ngram )
-    candidatesHandler = CandidatesXMLHandler( treat_sentence=treat_ngram )
+    handler = GenericXMLHandler( treat_entity=treat_ngram )
     
-    
-    parser.setContentHandler( corpusHandler ) 
+    parser.setContentHandler( handler )
     parser.parse( input_file )
-    input_file.close() 
-    print >> sys.stderr, str( word_counter ) + " words in " + arg[ 0 ]
-    print >> sys.stderr, str( sentence_counter ) + " sentences in " + arg[ 0 ]    
-    
+    input_file.close()
+    print >> sys.stderr, str( entity_counter ) + " entities in "   + arg[ 0 ]
+    print >> sys.stderr, str( word_counter )   + " words in "      + arg[ 0 ]
+    print >> sys.stderr, str( char_counter )   + " characters in " + arg[ 0 ]    
 except IOError, err :
     print >> sys.stderr, err
 except Exception, err :
     print >> sys.stderr, err
-    print >> sys.stderr, "You probably provided an invalid corpus file, " + \
-                         "please validate it against the DTD " + \
-                         "(mwetoolkit-corpus.dtd)"
+    print >> sys.stderr, "You probably provided an invalid XML file," +\
+                         " please validate it against the DTD " + \
+                         "(dtd/mwetoolkit-*.dtd)"
