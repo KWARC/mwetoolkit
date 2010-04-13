79aaf1033941714204c52bca644544ad48960691
ceramisch <ceramisch@c68e8873-8163-41b3-892e-79309705a67f>
2010-04-13 16:30:22 +0000

    Added License disclaimer

--- bin/xmlhandler/corpusXMLHandler.py
+++ bin/xmlhandler/corpusXMLHandler.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+################################################################################
+#
+# Copyright 2010 Carlos Ramisch
+#
+# corpusDTDHandler.py is part of mwetoolkit
+#
+# mwetoolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# mwetoolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mwetoolkit.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
 """
     This module provides the `CorpusXMLHandler` class. This class is a SAX 
     parser for XML documents that are valid corpora according to 
@@ -66,7 +88,7 @@ class CorpusXMLHandler( xml.sax.ContentHandler ) :
             else :
                 pos = WILDCARD
             # Add word to the sentence that is currently bein read
-            self.sentence.append_word( Word( surface, lemma, pos, [] ) )
+            self.sentence.append( Word( surface, lemma, pos, [] ) )
             
 
 ################################################################################
@@ -82,8 +104,4 @@ class CorpusXMLHandler( xml.sax.ContentHandler ) :
             # A complete sentence was read, call the callback function
             self.treat_sentence( self.sentence )  
      
-################################################################################
-
-if __name__ == "__main__" :
-    import doctest
-    doctest.testmod()    
+ 
