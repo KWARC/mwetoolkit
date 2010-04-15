903abc0ee9b2503abcd7b9611db2bd810514c0ae
ceramisch <ceramisch@c68e8873-8163-41b3-892e-79309705a67f>
2010-04-15 10:15:13 +0000

    Created test script and data sets for genia toy. Tests still need improvements, only three scripts are tested for the moment. Cleaned up some temporary files in the greek and genia experiments. Updated new dtds.

--- toy/genia/runAll.sh
+++ toy/genia/runAll.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-# This file runs the whole pipeline of MWT extraction with mwttoolkit. The
+# This file runs the whole pipeline of MWE extraction with mwetoolkit. The
 # result is a file called candidates.arff that can be treated by the WEKA
 # machine learning package. Some examples of models are in the "models" folder.
+# All the results are compared to standard results in the test folder, to verify
+# consistency.
 
 path_mwttoolkit="../../bin"
 python="/usr/bin/python"
 
 # INITIAL FILES
 
-# corpus.xml is a small subset of the Genia corpus containing 34 sentences 
+# corpus.xml is a small subset of the Genia corpus containing 500 sentences 
 # chosen randomly among the ~18K sentences of the corpus.
 
 corpus_name="corpus"
@@ -22,22 +24,39 @@ the_corpus="${corpus_name}.xml"
 the_patterns="patterns.xml"
 
 # A subset of the Genia reference list, which was created by looking at all
-# manually annotated MWTs in the corpus. The reference is the Gold Standard
+# manually annotated MWEs in the corpus. The reference is the Gold Standard
 # against which we compare our results.
 
 the_reference="reference.xml"
 
+echo "0) Statistics"
+echo "----Corpus----"
+${python} ${path_mwttoolkit}/wc.py -v ${the_corpus}
+echo "----Patterns----"
+${python} ${path_mwttoolkit}/wc.py -v ${the_patterns}
+echo "----Reference----"
+${python} ${path_mwttoolkit}/wc.py -v ${the_reference}
+
 echo "1) Generate the initial list of candidates using the POS patterns"
 
-${python} ${path_mwttoolkit}/candidates.py -s -p ${the_patterns} ${the_corpus} > candidates.xml
+${python} ${path_mwttoolkit}/candidates.py -s -v -n 2:3 ${the_corpus} > candidates.xml
+echo "----Initial candidates----"
+${python} ${path_mwttoolkit}/wc.py candidates.xml
 
-echo "2) Generate the index file"
+
+
+echo "2) Filter out candidates occurring less than threshold (2)"
+
+${python} ${path_mwttoolkit}/filter.py -v -t ${corpus_name}:2 candidates.xml > candidates-filter.xml
+echo "----Filtered candidates----"
+${python} ${path_mwttoolkit}/wc.py candidates-filter.xml
+
+
+echo "3) Generate the index file"
 
 ${python} ${path_mwttoolkit}/index.py -s -i corpus.index ${the_corpus}
 
-echo "3) Filter out candidates occurring less than threshold (2)"
 
-${python} ${path_mwttoolkit}/filter.py -t ${corpus_name}:2 candidates.xml > candidates-filter.xml
 
 echo "4) Count individual word frequencies"
 
@@ -62,4 +81,6 @@ ${python} ${path_mwttoolkit}/eval_automatic.py -r ${the_reference} feat-pos-am.x
 echo "9) Convert to WEKA format"
 
 ${python} ${path_mwttoolkit}/xml2arff.py feat-pos-am-class.xml > feat-pos-am-class.arff
+
+fi
 
--- toy/greek/runAll.sh
+++ toy/greek/runAll.sh
@@ -1,71 +1,63 @@
 #!/bin/sh
 
-# This file runs the whole pipeline of MWT extraction with mwttoolkit. The
-# result is a file called candidates.arff that can be treated by the WEKA
-# machine learning package. Some examples of models are in the "models" folder.
+# This file runs the whole pipeline of MWE extraction with mwetoolkit on a small
+# example corpus of Greek sentences.
 
 path_mwttoolkit="../../bin"
 python="/usr/bin/python"
 
 # INITIAL FILES
 
-# corpus.xml is a small subset of the Genia corpus containing 34 sentences 
-# chosen randomly among the ~18K sentences of the corpus.
-
+# The corpus is a small subset of 325 sentences taken from the Europarl Greek 
+# corpus
 corpus_name="gr_toy"
 
 the_corpus="${corpus_name}.xml"
 
-# patterns.xml is a set of POS patterns generated empirically to extract 
-# biomedical MWTs
-
+# gr_patterns.xml is a set of POS patterns defined by Evita Linardaki, cf. LREC
+# 2010 workshop on south-eastern european languages.
 the_patterns="gr_patterns.xml"
 
-# A subset of the Genia reference list, which was created by looking at all
-# manually annotated MWTs in the corpus. The reference is the Gold Standard
-# against which we compare our results.
+echo "0) Corpus statistics"
+#${python} ${path_mwttoolkit}/wc.py ${the_corpus}
 
-the_reference="reference.xml"
+${python} ${path_mwttoolkit}/tail.py -n 2000 ${the_corpus}
 
 echo "1) Generate the initial list of candidates using the POS patterns"
 
-${python} ${path_mwttoolkit}/candidates.py -p ${the_patterns} ${the_corpus} > candidates.xml
+#${python} ${path_mwttoolkit}/candidates.py -p ${the_patterns} ${the_corpus} > candidates.xml
+#${python} ${path_mwttoolkit}/wc.py candidates.xml
 
-echo "2) Generate the index file"
+#echo "2) Filter out candidates occurring less than threshold (2)"
 
-${python} ${path_mwttoolkit}/index.py -i corpus.index ${the_corpus}
+#${python} ${path_mwttoolkit}/filter.py -t ${the_corpus}:2 candidates.xml > candidates-filter.xml
+#${python} ${path_mwttoolkit}/wc.py candidates-filter.xml
 
-echo "3) Filter out candidates occurring less than threshold (2)"
+#echo "2) Generate the index files"
 
-${python} ${path_mwttoolkit}/filter.py -t ${the_corpus}:2 candidates.xml > candidates-filter.xml
+#${python} ${path_mwttoolkit}/index.py -i corpus.index ${the_corpus}
 
-echo "4) Count individual word frequencies"
 
-${python} ${path_mwttoolkit}/counter.py -i corpus.index candidates-filter.xml > candidates-filter-fCorpus.xml
+
+#echo "4) Count individual word frequencies"
+
+#${python} ${path_mwttoolkit}/counter.py -i corpus.index candidates-filter.xml > candidates-filter-fCorpus.xml
 
 #echo "5) Count words in Yahoo"
 
 #${python} ${path_mwttoolkit}/counter.py -y candidates-filter-fCorpus.xml > candidates-filter-fCorpus-fYahoo.xml
 
-echo "6) Extract descriptive features"
+#echo "6) Extract descriptive features"
 
-${python} ${path_mwttoolkit}/feat_pattern.py candidates-filter-fCorpus.xml > feat-pos.xml
+#${python} ${path_mwttoolkit}/feat_pattern.py candidates-filter-fCorpus.xml > feat-pos.xml
 
-echo "7) Extract Association Measure features"
+#echo "7) Extract Association Measure features"
 
-${python} ${path_mwttoolkit}/feat_association.py feat-pos.xml > feat-pos-am.xml
+#${python} ${path_mwttoolkit}/feat_association.py feat-pos.xml > feat-pos-am.xml
 
-echo "8) Sort the candidates according to Dice, and crop top-30 candidates"
+#echo "8) Sort the candidates according to Dice, and crop top-30 candidates"
 
-${python} ${path_mwttoolkit}/sort.py -f dice_gr_toy feat-pos-am.xml > tmp
-${python} ${path_mwttoolkit}/filter.py -c 30 tmp > feat-pos-am-dice.xml
-rm tmp
-
-#echo "8) Automatic evaluation"
-
-#${python} ${path_mwttoolkit}/eval_automatic.py -r ${the_reference} feat-pos-am.xml > feat-pos-am-class.xml
-
-#echo "9) Convert to WEKA format"
-
-#${python} ${path_mwttoolkit}/xml2arff.py feat-pos-am-class.xml > feat-pos-am-class.arff
+#${python} ${path_mwttoolkit}/sort.py -f dice_gr_toy feat-pos-am.xml > tmp
+#${python} ${path_mwttoolkit}/filter.py -c 30 tmp > feat-pos-am-dice.xml
+#rm tmp
 
