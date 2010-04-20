bdecea40dc89cf7ea64ed66cc6b63e96a5bedc9f
ceramisch <ceramisch@c68e8873-8163-41b3-892e-79309705a67f>
2010-04-20 15:20:37 +0000

    1) Added -m parameter, allows to chose which measures are calculated on the frequencies. 2) Implemented Log-likelihood for bigrams (generic n-grams are supported by script but not by current dtds) 3) Tested, refactored and documented some functions (notably contingency_tables)

--- bin/feat_association.py
+++ bin/feat_association.py
@@ -41,6 +41,7 @@
 import sys
 import xml.sax
 import math
+import pdb
 
 from xmlhandler.candidatesXMLHandler import CandidatesXMLHandler
 from xmlhandler.classes.feature import Feature
@@ -60,6 +61,8 @@ python %(program)s <candidates.xml>
 supported_measures = [ "mle", "t", "pmi", "dice", "ll" ]
 corpussize_dict = {}
 measures = supported_measures
+# TODO: Parametrable combine function
+heuristic_combine = lambda l : sum( l ) / len( l ) # Arithmetic mean
      
 ################################################################################     
        
@@ -111,7 +114,96 @@ def treat_candidate( candidate ) :
 
 ################################################################################     
 
-def calculate_ams( f, m_list, N, corpus_name ) :
+def contingency_tables( bigram_freqs, unigram_freqs, N, corpus_name ):
+    """
+        Given an ngram (generic n) w_1 ... w_n, the input is a couple of lists
+        containing integer frequencies, the output is a couble of lists with
+        contingency tables. The first list contains bigram frequencies
+        [ f(w_1 w_2), f(w_2 w_3), ..., f(w_n-1 w_n) ]. The second list contains
+        unigram frequencies [ f(w_1), f(w_2), ..., f(w_n) ]. While the first
+        list contains n-1 elements, the second list contains n elements. The
+        result is a couple of lists with contingency tables, the first
+        corresponds to the observed frequencies, the second to expected
+        frequencies. The contingency tables are 2D lists that contain the 4
+        possible outcomes for the occurrence of a bigram, i.e. c(w1 w2),
+        c(w1 ~w2), c(~w1 w2) and c(~w1 ~w2), where "~w" means "any word but w".
+        Observed contingency tables are exact calculations based on simple
+        set operations (intersection, difference). The expected frequencies are
+        calculated using maximum likelihood for independent events (e.g. the
+        occurrence of w1 does not change the probability of the occurrence of w2
+        or of ~w2 imediately after w1, also noted P(w2|w1)=P(w2)).
+
+        @param bigram_freqs List of integers representing bigram frequencies.
+        Notice that no bigram can occur more than the words that is contains.
+        Any inconsistency will be automatically corrected and output as a
+        warning. This list should contain n-1 elements.
+
+        @param unigram_freqs List of integers representing unigram (word)
+        frequencies. This list should have n elements.
+
+        @param corpus_name The name of the corpus from which frequencies were
+        drawn. This is only used in verbose mode to provide friendly output
+        messages.
+
+        @return a couple (observed, expected), where observed and expected are
+        lists, both of size n-1, and each cell of each list contains a 2x2 table
+        with observed and expected contingency tables for the bigrams given as
+        input.
+    """
+    observed = []
+    expected = []
+    if len(bigram_freqs) != (len(unigram_freqs)-1) :
+        print >> sys.stderr, "WARNING: Invalid unigram/bigram frequencies " +\
+                             "passed to calculate_negations function"
+        return None
+    # 1) Verify that all the frequencies are valid
+    for i in range( len( bigram_freqs ) ) :
+        if bigram_freqs[ i ] > unigram_freqs[ i ] or \
+           bigram_freqs[ i ] > unigram_freqs[ i + 1 ] :
+            print >> sys.stderr, "WARNING: " + corpus_name + " unigrams " +\
+                                 "must occur at least as much as bigram."
+        if bigram_freqs[ i ] > unigram_freqs[ i ] :
+            print >> sys.stderr, "Automatic correction: " + \
+                                 str( unigram_freqs[ i ] ) + " -> " + \
+                                 str( bigram_freqs[ i ] )
+            unigram_freqs[ i ] = bigram_freqs[ i ]
+        if bigram_freqs[ i ] > unigram_freqs[ i + 1 ] :            
+            print >> sys.stderr, "Automatic correction: " + \
+                                 str( unigram_freqs[ i + 1 ] ) + " -> " + \
+                                 str( bigram_freqs[ i ] )
+            unigram_freqs[ i + 1 ] = bigram_freqs[ i ]
+    # 2) Calculate negative freqs 
+    for i in range( len( bigram_freqs ) ) :        
+        o = [ 2 * [ -1 ], 2 * [ -1 ] ]
+        e = [ 2 * [ -1 ], 2 * [ -1 ] ]
+        cw1 = unigram_freqs[ i ]
+        cw2 = unigram_freqs[ i + 1 ]
+        cw1w2 = bigram_freqs[ i ]
+        o[ 0 ][ 0 ] = cw1w2
+        e[ 0 ][ 0 ] = expect( [ cw1, cw2 ], N )
+        o[ 0 ][ 1 ] = cw1 - cw1w2
+        e[ 0 ][ 1 ] = expect( [ cw1, N - cw2 ], N )
+        o[ 1 ][ 0 ] = cw2 - cw1w2
+        e[ 1 ][ 0 ] = expect( [ N - cw1, cw2 ], N )
+        o[ 1 ][ 1 ] = N - len( unigram_freqs )  + 1 - cw1w2 #exact nb of bigrams
+        e[ 1 ][ 1 ] = expect( [ N - cw2, N - cw2 ], N )
+        observed.append( o )
+        expected.append( e )
+    return (observed, expected)
+
+################################################################################
+
+def expect( m_list, N ):
+    """
+    """
+    result = 1.0
+    for i in range(len(m_list)) :
+        result *= m_list[ i ] / N
+    return result * ( N - len(m_list) + 1 )
+
+################################################################################
+
+def calculate_ams( o, m_list, N, corpus_name ) :
     """
         Given a joint frequency of the ngram, a list of individual frequencies,
         a corpus size and a corpus name, generates a list of `Features`, each
@@ -133,32 +225,53 @@ def calculate_ams( f, m_list, N, corpus_name ) :
     """
     # N is never null!!!
     # m_list is never empty!!!
+    global measures, heuristic_combine
+    feats = []
     f_sum = 0
     n = len( m_list )
-    f_null = 1 / math.pow( N, n - 1 )
-    for m_i in m_list :
-        f_null *= m_i
-        f_sum += m_i
-    
-    mle = f / N  
-    if f_null != 0.0 and f != 0:
-        pmi = math.log( f / f_null, 2 ) + (n - 1) * math.log( N, 2 )
-    else :
-        pmi = 0.0
-    if f != 0.0 :
-        t = ( f - f_null ) / math.sqrt( f )
-    else :
-        t = 0.0
-    if f_sum != 0.0 :
-        dice = ( n * f ) / f_sum
-    else :
-        dice = 0.0
+    e = expect( m_list, N )
+    if "mle" in measures :
+        feats.append( Feature( "mle_" + corpus_name, o / N ) )
+    if "pmi" in measures :
+        if e != 0 and o != 0:
+            pmi = math.log( o / e, 2 )
+        else :
+            pmi = 0.0
+        feats.append( Feature( "pmi_" + corpus_name, pmi ) )
+    if "t" in measures :
+        if o != 0.0 :
+            t = ( o - e ) / math.sqrt( o )
+        else :
+            t = 0.0
+        feats.append( Feature( "t_" + corpus_name, t ) )
+    if "dice" in measures :
+        if sum( m_list ) != 0.0 :
+            dice = ( n * o ) / sum( m_list )
+        else :
+            dice = 0.0
+        feats.append( Feature( "dice_" + corpus_name, dice ) )
+    if "ll" in measures :
+        #pdb.set_trace()
+        if len( m_list ) == 2 :
+            # Contingency tables observed, expected
+            ( ct_os, ct_es ) = contingency_tables( [o], m_list, N, corpus_name )
+            ll_list  = [] # Calculation is suitable for generic ngrams
+            for (ct_o, ct_e) in map( None, ct_os, ct_es ) :
+                ll = 0.0
+                for i in range( 2 ) :
+                    for j in range( 2 ) :
+                        if ct_o[i][j] != 0.0 :
+                            
+                            ll += ct_o[i][j] * ( math.log( ct_o[i][j], 2 ) -\
+                                                 math.log( ct_e[i][j], 2 ) )
 
-    feats = []
-    feats.append( Feature( "mle_" + corpus_name, mle ) )
-    feats.append( Feature( "pmi_" + corpus_name, pmi ) )
-    feats.append( Feature( "t_" + corpus_name, t ) )
-    feats.append( Feature( "dice_" + corpus_name, dice ) )    
+                ll_list .append( ll )
+            ll_final = heuristic_combine( ll_list )
+        else :
+            print >> sys.err, "WARNING: log-likelihood is only implemented "+\
+                              "for 2-grams. Defaults to 0.0 for n>2"
+            ll_final = 0.0
+        feats.append( Feature( "ll_" + corpus_name, ll_final ) )
     return feats
 
 ################################################################################
@@ -222,8 +335,8 @@ try :
     
 except IOError, err :
     print >> sys.stderr, err
-except Exception, err :
-    print >> sys.stderr, err
-    print >> sys.stderr, "You probably provided an invalid candidates file," + \
-                         " please validate it against the DTD " + \
-                         "(dtd/mwetoolkit-candidates.dtd)"
+#except Exception, err :
+#    print >> sys.stderr, err
+#    print >> sys.stderr, "You probably provided an invalid candidates file," + \
+#                         " please validate it against the DTD " + \
+#                         "(dtd/mwetoolkit-candidates.dtd)"
