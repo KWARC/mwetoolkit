b7d79831e3419060467539e33aa0e641e39cdc5e
vbuaraujo <vbuaraujo@c68e8873-8163-41b3-892e-79309705a67f>
2011-09-01 00:25:21 +0000

    More changes to LocalMaxs.

--- bin/localmaxs-1.py
+++ bin/localmaxs-1.py
@@ -1,12 +1,25 @@
 import sys
+import tempfile
 from libs.indexlib import Index
 
-def extract(index, array_name, gluefun, min_ngram=2, max_ngram=8, outstream=sys.stdout):
+from xmlhandler.classes.__common import WILDCARD, \
+                                        TEMP_PREFIX, \
+                                        TEMP_FOLDER, \
+                                        XML_HEADER, \
+                                        XML_FOOTER
+def make_temp_file():
+	return tempfile.NamedTemporaryFile(prefix=TEMP_PREFIX, dir=TEMP_FOLDER)
+
+def python_hates_the_world(sentence_id, position, key, glue):
+	print sentence_id, position, key, glue
+
+def extract(index, array_name, gluefun, min_ngram=2, max_ngram=8, dumpfun=python_hates_the_world):
 	sufarray = index.arrays[array_name]
 	sentence_id = 0
 	pos = 0
 	corpus_length = len(sufarray.corpus)
 	corpus_size = index.metadata['corpus_size']  # corpus_size does not count sentence separators.
+	result_gluevals = {}
 
 	while pos < corpus_length:
 		if sentence_id%10 == 0:
@@ -17,6 +30,7 @@ def extract(index, array_name, gluefun, min_ngram=2, max_ngram=8, outstream=sys.
 			sentence_length += 1
 
 		gluevals = {}
+		positions = {}
 		select = {}
 
 		for ngram_size in range(1, max_ngram+1):
@@ -26,6 +40,7 @@ def extract(index, array_name, gluefun, min_ngram=2, max_ngram=8, outstream=sys.
 				glue = gluefun(gluevals, sufarray, corpus_size, key)
 				gluevals[key] = glue
 				select[key] = True
+				positions[key] = range(i, i+ngram_size)
 
 				if ngram_size >= 2:
 					for subkey in [key[0:-1], key[1:]]:
@@ -34,21 +49,15 @@ def extract(index, array_name, gluefun, min_ngram=2, max_ngram=8, outstream=sys.
 						else:
 							select[subkey] = False
 
-		# Print results.
+		# Save results.
 		for key in select:
 			if len(key) > 1 and len(key) < max_ngram and select[key]:
-				try:
-					print >>outstream, ' '.join([sufarray.symbols.number_to_symbol[num] for num in key]), "(glue %s)"%gluevals[key]
-				except Exception, err:
-					print key
-					for num in key:
-						if num > len(sufarray.symbols.number_to_symbol):
-							print "Tiu ne forgesas: %d" % num
+				dumpfun(sentence_id, positions[key], key, gluevals[key])
 
-		print >>outstream, "---"
-		outstream.flush()
 		sentence_id+=1
 		pos += sentence_length + 1
+
+	return result_gluevals
 
 def scp_glue(gluevals, sufarray, corpus_size, key):
 	main_prob = ngram_prob(sufarray, corpus_size, key)
@@ -77,9 +86,13 @@ def ngram_prob(sufarray, corpus_size, key):
 	return count / float(corpus_size)
 
 
-# Test.
-h = Index("/home/vitor/BolsaPLN/genia/index/corpus")
-h.load_main()
-out = open("/tmp/outout", "w")
-extract(h, 'lemma', scp_glue, outstream=out)
-out.close()
+def main(args):
+	tempfile = make_temp_file()
+	def dump(sentence_id, position, key, glue):
+		print >>tempfile, "%s|%s|%s|%s" % (sentence_id, ' '.join(map(str, position)), ' '.join(map(str, key)), glue)
+
+	index = Index(args[0])
+	index.load_main()
+	extract(index, 'lemma', scp_glue, dumpfun=dump)
+
+main(sys.argv[1:])
