2d7feb9e0aa6102de29d31b410f76c01380e91da
ceramisch <ceramisch@c68e8873-8163-41b3-892e-79309705a67f>
2011-12-15 13:18:32 +0000

    Modifications unknown

--- bin/treetagger2xml.sh
+++ bin/treetagger2xml.sh
@@ -8,4 +8,6 @@
 
 cat $@ |
 sed -e 's/\&/\&amp;/g' -e 's/"/\&quot;/g' -e 's/>/\&gt;/g' -e 's/</\&lt;/g' -e "s/\t\t/\t/g" |
-awk 'BEGIN{ print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"; print "<!DOCTYPE corpus SYSTEM \"dtd/mwetoolkit-corpus.dtd\">"; print "<corpus>"; s_id=0; printf ( "<s s_id=\"%d\">", s_id++ ); FS="\t" }{ lemma = $3; if ( lemma == "&lt;unknown&gt;" ){ lemma = $1 } printf( "<w surface=\"%s\" lemma=\"%s\" pos=\"%s\"/>",$1,lemma,$2) }/SENT/{ printf( "</s>\n<s s_id=\"%d\">", s_id++ ); getline; }END{ printf( "</s>\n" ); print "</corpus>";  }' 
+awk '
+BEGIN{ 
+	print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"; print "<!DOCTYPE corpus SYSTEM \"dtd/mwetoolkit-corpus.dtd\">"; print "<corpus>"; s_id=0; printf ( "<s s_id=\"%d\">", s_id++ ); FS="\t" }{ lemma = $3; if ( lemma == "&lt;unknown&gt;" ){ lemma = $1 } printf( "<w surface=\"%s\" lemma=\"%s\" pos=\"%s\"/>",$1,lemma,$2) }/SENT/{ printf( "</s>\n<s s_id=\"%d\">", s_id++ ); getline; }END{ printf( "</s>\n" ); print "</corpus>";  }' 
