597b026bebc48069e37cc9c8ffa2ea8f63b0a223
ceramisch <ceramisch@c68e8873-8163-41b3-892e-79309705a67f>
2012-02-10 15:47:32 +0000

    Still trying to fix this readlink MAC OS problem...

--- toy/genia/testAll.sh
+++ toy/genia/testAll.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
-set -e           # Exit on errors...
+#set -e           # Exit on errors...
 exec </dev/null  # Don't hang if a script tries to read from stdin.
 testgread=`which greadlink`   # Adaptation for MAC OS, because readlink for
 if [ -z $testgread ]; then    # MAC OS does not accept the -f opt
-	alias greadlink="readlink"; 
+	DIR="$(readlink -f $(dirname "$0"))"
+else
+	DIR="$(greadlink -f $(dirname "$0"))"
 fi
 
-DIR="$(greadlink -f $(dirname "$0"))"
 TOOLKITDIR="$DIR/../.."
 OUTDIR="$DIR/output"
 
