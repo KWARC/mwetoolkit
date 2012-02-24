769afff836a782c3445beaf4905fa746d5ca4403
ceramisch <ceramisch@c68e8873-8163-41b3-892e-79309705a67f>
2012-02-24 19:10:18 +0000

    Made the treetagger sentence splitting language independent and added an explanation to the usage and documentation

--- bin/treetagger2xml.py
+++ bin/treetagger2xml.py
@@ -3,7 +3,7 @@
 
 ################################################################################
 #
-# Copyright 2010 Carlos Ramisch
+# Copyright 2010-2012 Carlos Ramisch, Vitor de Araujo
 #
 # treetagger2xml.py is part of mwetoolkit
 #
@@ -50,18 +50,28 @@ python %(program)s OPTIONS <files.xml>
 OPTIONS may be:
 
 -o OR --original-split
-	Keep the original sentence splitting of the input file. If this option is
-	activated, you should add a sentence delimiter "</s>" at the end of each
-	sentence before feeding the text into treetagger. This tag will be ignored
-	by the tagger and then used to retrieve the original sentence splitting. 
-	This otpion is particularly useful when dealing with parellel corpora in 
-	which the sentence alignment cannot be messed up by the tagger.
+    Keep the original sentence splitting of the input file. If this option is
+    activated, you should add a sentence delimiter "</s>" at the end of each
+    sentence before feeding the text into treetagger. This tag will be ignored
+    by the tagger and then used to retrieve the original sentence splitting. 
+    This otpion is particularly useful when dealing with parellel corpora in 
+    which the sentence alignment cannot be messed up by the tagger.
+
+-s <sent> OR --sentence <sent>
+    Name of the POS tag that the treetagger uses to separate sentences. Please,
+    specify this if you're not using --original segmentation. Defaults to "SENT"
+    which is the name of the tag used to indicate English sentence splitting.
+
+    ATTENTION: when working in a language other than English, please specify one
+    of the two options, -s or -o, otherwise the result may look like a corpus
+    with a  single (very long) line.
 
 %(common_options)s
 
     The <files.xml> file(s) must be valid XML (dtd/mwetoolkit-*.dtd).
 """    
 original_split = False
+sent_split = "SENT"
 
 ################################################################################     
        
@@ -79,68 +89,72 @@ def treat_options( opts, arg, n_arg, usage_string ) :
         the wrong parameters or options.
     """
     global original_split
+    global sent_split
     for ( o, a ) in opts:
         if o in ( "-o", "--original-split" ) : 
             original_split = True
+        if o in ( "-s", "--sentence" ) : 
+            sent_split = a            
                 
     treat_options_simplest( opts, arg, n_arg, usage_string )
 
 ################################################################################     
 
 def escape( line ) :
-	"""
-		Replaces the escaped characters " & < > ` by the corresponding XML 
-		entities.
-		
-		@param line The string containing characters that may be escaped
-		
-		@return The same string with the special XML characters replaced by the
-		corresponding entitites
-	"""
-	result = line
-	result = result.replace( "&", "&amp;" )
-	result = result.replace( "\"", "&quot;" )
-	result = result.replace( "<", "&lt;" )
-	result = result.replace( ">", "&gt;" )
-	result = result.replace( "'", "&apos;" )
-	return result
-	
+    """
+        Replaces the escaped characters " & < > ` by the corresponding XML 
+        entities.
+        
+        @param line The string containing characters that may be escaped
+        
+        @return The same string with the special XML characters replaced by the
+        corresponding entitites
+    """
+    result = line
+    result = result.replace( "&", "&amp;" )
+    result = result.replace( "\"", "&quot;" )
+    result = result.replace( "<", "&lt;" )
+    result = result.replace( ">", "&gt;" )
+    result = result.replace( "'", "&apos;" )
+    return result
+    
 ################################################################################
 
 def transform_format( in_file ) :
-	"""
-		Reads an input file and converts it into mwetoolkit corpus XML format, 
-		printing the XML file to stdout.
-		
-		@param in_file The file in TreeTagger TAB-separated format, one word per
-		line, each word is in format "surface\tpos\tlemma". Optional sentence
-		separators "</s>" may also constitute a word on a line.
-	"""
-	global original_split
-	#print original_split
-	s_id = 0
-	new_sent = True
-	words = []
-	for line in in_file.readlines() :
-		line = line.strip()
-		if new_sent and line != "</s>" :
-			print "<s s_id=\"" + str( s_id ) + "\">",
-			s_id = s_id + 1
-			new_sent = False
-			#pdb.set_trace()
-		if line == "</s>" and original_split and words :
-			print "</s>"
-			new_sent = True
-			words = []
-		elif line != "</s>" :
-			line = escape( line )
-			(surface, pos, lemma) = line.split("\t")
-			print "<w surface=\"" + surface + "\" pos=\"" + pos + "\" lemma=\"" + lemma + "\"/>",
-			words.append( surface )
-			if pos == "SENT" and not original_split and words :
-				print "</s>"
-				new_sent = True
-				words = []	
+    """
+        Reads an input file and converts it into mwetoolkit corpus XML format, 
+        printing the XML file to stdout.
+        
+        @param in_file The file in TreeTagger TAB-separated format, one word per
+        line, each word is in format "surface\tpos\tlemma". Optional sentence
+        separators "</s>" may also constitute a word on a line.
+    """
+    global original_split
+    global sent_split
+    s_id = 0
+    new_sent = True
+    words = []
+    for line in in_file.readlines() :
+        line = line.strip()
+        if new_sent and line != "</s>" :
+            print "<s s_id=\"" + str( s_id ) + "\">",
+            s_id = s_id + 1
+            new_sent = False
+            #pdb.set_trace()
+        if line == "</s>" and original_split and words :
+            print "</s>"
+            new_sent = True
+            words = []
+        elif line != "</s>" :
+            line = escape( line )
+            (surface, pos, lemma) = line.split("\t")
+            print "<w surface=\"" + surface + "\" pos=\"" + pos + "\" lemma=\"" + lemma + "\"/>",
+            words.append( surface )
+            # Only works for english if option -s not specified
+            if pos == sent_split and not original_split and words :
+                print "</s>"
+                new_sent = True
+                words = []    
 
 ################################################################################     
 # MAIN SCRIPT
