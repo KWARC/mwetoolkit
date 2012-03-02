4a78757f885728a439785d23c7517cb1532ded49
vbuaraujo <vbuaraujo@c68e8873-8163-41b3-892e-79309705a67f>
2012-03-02 14:55:23 +0000

    Tests for thee.

--- toy/genia/testAll.sh
+++ toy/genia/testAll.sh
@@ -14,7 +14,7 @@ OUTDIR="$DIR/output"
 
 run() {
 	local script="$1"; shift
-	python "$TOOLKITDIR/bin/$script" "$@"
+	python "$TOOLKITDIR/bin/$script" --debug "$@"
 }
 
 dotest() {
@@ -51,14 +51,22 @@ main() {
 
 	while [[ $# -gt 0 ]]; do
 		case "$1" in
-			-s) tests_to_skip="$2"; shift ;;
+			-s) tests_to_skip="$(($2-1))"; shift ;;
 			*) echo "Unknown option \"$1\"!" >&2; return 1 ;;
 		esac
 		shift
 	done
 
+	dotest "Conversion from TreeTagger to XML" \
+		'run treetagger2xml.py -v "$DIR/corpus-treetagger.txt" >corpus-ptb-tags.xml' \
+		true
+
+	dotest "POS tag simplification" \
+		'run changepos.py -v corpus-ptb-tags.xml >corpus.xml' \
+		true
+
 	dotest "Corpus indexing" \
-		'run index.py -v -i corpus "$DIR/corpus.xml"' \
+		'run index.py -v -i corpus "corpus.xml"' \
 		'true'
 
 	dotest "Extraction from index" \
@@ -66,7 +74,7 @@ main() {
 		true
 
 	dotest "Extraction from XML" \
-		'run candidates.py -s -v -p "$DIR/patterns.xml" "$DIR/corpus.xml" >candidates-from-corpus.xml' \
+		'run candidates.py -s -v -p "$DIR/patterns.xml" "corpus.xml" >candidates-from-corpus.xml' \
 		true
 
 	dotest "Comparison of candidate extraction outputs" \
@@ -78,9 +86,54 @@ main() {
 		true
 
 	dotest "Association measures" \
-		'run feat_association.py -v -m "mle:pmi:t:dice" candidates-counted.xml >candidates-featureful.xml' \
+		'run feat_association.py -v -m "mle:pmi:t:dice:ll" candidates-counted.xml >candidates-featureful.xml' \
 		true
 
+	dotest "Evaluation" \
+		'run eval_automatic.py -v -r "$DIR/reference.xml" -g candidates-featureful.xml >eval.xml 2>eval-stats.txt' \
+		true
+
+	dotest "Mean Average Precision" \
+		'run map.py -v -f "mle_corpus:pmi_corpus:t_corpus:dice_corpus:ll_corpus" eval.xml >map.txt' \
+		true
+
+	for format in csv arff evita owl ucs; do
+		dotest "Conversion from XML to $format" \
+			"run xml2$format.py -v candidates-featureful.xml >candidates-featureful.$format" 2>>warnings-$format.txt \
+			true
+	done
+
+	dotest "Take first 50 candidates" \
+		'run head.py -v -n 50 candidates-featureful.xml >candidates-featureful-head.xml' \
+		true
+
+	dotest "Take last 50 candidates" \
+		'run tail.py -v -n 50 candidates-featureful.xml >candidates-featureful-tail.xml' \
+		true
+
+	dotest "Take first 50 corpus sentences" \
+		'run head.py -v -n 50 corpus.xml >corpus-head.xml' \
+		true
+
+	dotest "Take last 50 corpus sentences" \
+		'run tail.py -v -n 50 corpus.xml >corpus-tail.xml' \
+		true
+
+	for base in candidates-featureful corpus; do
+		for suffix in '' -head -tail; do
+			dotest "Word count for $base$suffix" \
+				"run wc.py $base$suffix.xml 2>&1 | tee wc-$base$suffix.txt" \
+				true
+		done
+	done
+
+	dotest "Removal of duplicated candidates" \
+		'run uniq.py candidates-featureful.xml >candidates-uniq.xml' \
+		true
+
+	dotest "Removal of duplicated candidates ignoring POS" \
+		'run uniq.py candidates-featureful.xml >candidates-uniq-nopos.xml' \
+		true
 }
 
 main "$@"
