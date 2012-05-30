841567722b47d32644eba7ca0e48c36d2af93b2a
ceramisch <ceramisch@c68e8873-8163-41b3-892e-79309705a67f>
2012-05-30 13:00:24 +0000

    Adaptation of sed to MacOS version

--- src/indexer/genheader.sh
+++ src/indexer/genheader.sh
@@ -6,16 +6,5 @@ for file; do
 	[[ $file == */* ]] && dir="${file%/*}"
 	outname="$dir/autoheaders/$barename.h"
 
-	sed -ne '
-		/^typedef struct .*{/,/^[[:space:]]*}/p
-		/^struct .*{/,/^[[:space:]]*};/p
-		/^union [^[:space:]].*{/,/^[[:space:]]*};/p
-		/^typedef .*;/p
-		/^#define .*\\$/,/[^\]$/p
-		/^#define .*\\$/!{/^[[:space:]]*#[[:alnum:]]/p}
-		/^typedef\|^struct\|^union/!s/^\([A-Za-z][^{]*\)=.*/extern \1;/p
-		/^typedef\|^struct\|^union/!s/^\([A-Za-z][^{]*[^ {]\)[[:space:]]*{$/\1;/gp
-		/^typedef\|^struct\|^union/!{
-			/^\([A-Za-z][^{]*[^ {]\)[[:space:]]*,$/,/.*{$/{ s/[[:space:]]*{$/;/g; p } }
-	' <"$file" >"$outname"
+	sed -ne '/^typedef struct .*{/,/^[[:space:]]*}/p;/^struct .*{/,/^[[:space:]]*};/p;/^union [^[:space:]].*{/,/^[[:space:]]*};/p;/^typedef .*;/p;/^#define .*\\$/,/[^\]$/p;/^#define .*\\$/!{/^[[:space:]]*#[[:alnum:]]/p};/^typedef\|^struct\|^union/!s/^\([A-Za-z][^{]*\)=.*/extern \1;/p;/^typedef\|^struct\|^union/!s/^\([A-Za-z][^{]*[^ {]\)[[:space:]]*{$/\1;/gp;/^typedef\|^struct\|^union/!{;/^\([A-Za-z][^{]*[^ {]\)[[:space:]]*,$/,/.*{$/{ s/[[:space:]]*{$/;/g; p } }' <"$file" >"$outname"
 done
