330766b1254b9657b2db12a8104543c08385df55
ceramisch <ceramisch@c68e8873-8163-41b3-892e-79309705a67f>
2013-04-11 11:10:53 +0000

    Global refactoring of C Makefiles -> it was over-complex and didn't work with Mac. Also added support for doxygen documentation

--- src/indexer/autoheaders/main.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#import "base.h"
-#import "readline.h"
-#import "suffixarray.h"
-int main(int argc, char **argv);
--- src/indexer/autoheaders/readline.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include "base.h"
-#include <string.h>
-char *readline(FILE *stream);
-char *copystring(char *str);
