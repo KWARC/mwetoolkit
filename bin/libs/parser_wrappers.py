b2cb87ceca364f9761913baee9d776630bc8540f
ceramisch <ceramisch@c68e8873-8163-41b3-892e-79309705a67f>
2014-09-05 14:38:07 +0000

    Change GNU licence (minor)

--- bin/libs/parser_wrappers.py
+++ bin/libs/parser_wrappers.py
@@ -5,7 +5,7 @@
 #
 # Copyright 2014 Silvio Ricardo Cordeiro
 #
-# printer.py is part of mwetoolkit
+# parser_wrappers.py is part of mwetoolkit
 #
 # mwetoolkit is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
